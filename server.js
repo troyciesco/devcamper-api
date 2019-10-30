25e8cdd83008df3d5964d0f1dd04972065202138
Troy Ciesco <tmciesco@gmail.com>
2019-10-30 00:27:05 -0400

    added controllers and routes

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require("express")
 const dotenv = require("dotenv")
 
+// Route files
+const bootcamps = require("./routes/bootcamps")
+
 // Load env vars
 dotenv.config({ path: "./config/config.env" })
 
 const app = express()
 
-app.get("/", (req, res) => {
-	//res.send("hello from express")
-	res.status(200).json({ name: "Troy" })
-})
+// Mount routers
+app.use("/api/v1/bootcamps", bootcamps)
 
 const port = process.env.PORT || 5000
 
