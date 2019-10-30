46cab839a1abc6fc381ce5930343dcb8ed58c979
Troy Ciesco <tmciesco@gmail.com>
2019-10-30 00:09:34 -0400

    initial commit

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ dotenv.config({ path: "./config/config.env" })
 
 const app = express()
 
+app.get("/", (req, res) => {
+	//res.send("hello from express")
+	res.status(200).json({ name: "Troy" })
+})
+
 const port = process.env.PORT || 5000
 
 app.listen(port, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`))
