d9371e06e45ac6f78e22fa6578b5dd43aebd3fc9
Troy Ciesco <tmciesco@gmail.com>
2019-11-03 14:56:18 -0500

    added advanced results middleware

--- controllers/courses.js
+++ controllers/courses.js
@@ -8,86 +8,13 @@ const Bootcamp = require("../models/Bootcamp")
 // @route   GET /api/v1/bootcamps/:bootcampId/courses
 // @access  Public
 exports.getCourses = asyncHandler(async (req, res, next) => {
-	let query
-
 	if (req.params.bootcampId) {
-		query = Course.find({ bootcamp: req.params.bootcampId })
+		const courses = await Course.find({ bootcamp: req.params.bootcampId })
+
+		return res.status(200).json({ success: true, count: courses.length, data: courses })
 	} else {
-		// query = Course.find().populate("bootcamp")
-		query = Course.find().populate({
-			path: "bootcamp",
-			select: "name description"
-		})
+		res.status(200).json(res.advancedResults)
 	}
-
-	const courses = await query
-
-	res.status(200).json({
-		success: true,
-		count: courses.length,
-		data: courses
-	})
-	// // Copy req.query
-	// const reqQuery = { ...req.query }
-
-	// // Fields to exclude from matching
-	// const removeFields = ["select", "sort", "page", "limit"]
-
-	// // Loop over removeFields and delete them from reqQuery
-	// removeFields.forEach(param => delete reqQuery[param])
-
-	// // Create query string
-	// let queryString = JSON.stringify(reqQuery)
-
-	// // Create operators ($gt, $gte, etc.)
-	// queryString = queryString.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`)
-
-	// // Finding resource
-	// query = Course.find(JSON.parse(queryString))
-
-	// // Select Fields
-	// if (req.query.select) {
-	// 	const fields = req.query.select.split(",").join(" ")
-	// 	query = query.select(fields)
-	// }
-
-	// // Sort
-	// if (req.query.sort) {
-	// 	const sortBy = req.query.sort.split(",").join(" ")
-	// 	query = query.sort(sortBy)
-	// } else {
-	// 	query = query.sort("-createdAt")
-	// }
-
-	// // Pagination
-	// const page = parseInt(req.query.page, 10) || 1
-	// const limit = parseInt(req.query.limit, 10) || 1
-	// const startIndex = (page - 1) * limit
-	// const endIndex = page * limit
-	// const total = await Course.countDocuments()
-
-	// query.skip(startIndex).limit(limit)
-
-	// // Excecute query
-	// const courses = await query
-
-	// // Pagination result
-	// const pagination = {}
-	// if (endIndex < total) {
-	// 	pagination.next = {
-	// 		page: page + 1,
-	// 		limit
-	// 	}
-	// }
-
-	// if (startIndex > 0) {
-	// 	pagination.prev = {
-	// 		page: page - 1,
-	// 		limit
-	// 	}
-	// }
-
-	// res.status(200).json({ success: true, count: courses.length, pagination, data: courses })
 })
 
 // @desc    Get single course
